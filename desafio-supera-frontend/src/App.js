a219f0e0c156c5c9e7eed34999c901c4e70d3526
marcondesjr96 <jrmoriaproducoes1996@gmail.com>
2023-07-17 03:59:03 -0300

    Fazendo requisição com os filtros

--- desafio-supera-frontend/src/App.js
+++ desafio-supera-frontend/src/App.js
@@ -1,18 +1,51 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
 import SearchComponent from './componentes/SearchComponent';
 import TableComponent from './componentes/TableComponent';
+import axios from 'axios';
 
 export default function App() {
-  return (
-    <div className="app">
-      <SearchComponent />
-      <br/>
-      <br/>
-      <br/>
+  const [searchData, setSearchData] = useState([]);
 
-      <TableComponent />
+  const handleSearch = async (accountId, dataInicial, dataFinal, nomeOperadorTransacao) => {
+    try {
+      const params = {
+        page: 0,
+        size: 8,
+        contaId: accountId
+      };
+  
+      if (dataInicial) {
+        params.dataInicial = dataInicial;
+      }
+  
+      if (dataFinal) {
+        params.dataFinal = dataFinal;
+      }
+  
+      if (nomeOperadorTransacao) {
+        params.nomeOperadorTransacao = nomeOperadorTransacao;
+      }
+  
+
+      const response = await axios.get('http://localhost:8080/api/transferencia/account', {
+        params: params
+      });
+
+      setSearchData(response.data.content);
+    } catch (error) {
+      console.error('Erro ao buscar transferências:', error);
+    }
+  };
+
+  return (
+    <div>
+      <SearchComponent onSearch={handleSearch} />
+      <br />
+      <br />
+      <br />
+      <TableComponent searchData={searchData} />
     </div>
   );
 }
--- desafio-supera-frontend/src/componentes/SearchComponent.js
+++ desafio-supera-frontend/src/componentes/SearchComponent.js
@@ -1,23 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { format } from 'date-fns';
 
-const SearchComponent = () => {
+import './SearchComponent.css';
+
+
+const SearchComponent = ({ onSearch }) => {
+  const [accountId, setAccountId] = useState('');
+  const [isAccountIdValid, setIsAccountIdValid] = useState(false);
+  const [dataInicio, setDataInicio] = useState('');
+  const [dataFim, setDataFim] = useState('');
+  const [nomeOperadorTransacao, setNomeOperadorTransacao] = useState('');
+
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setAccountId(value);
+    setIsAccountIdValid(value !== '');
+  };
+
+    const handleDataInicioChange = (event) => {
+      setDataInicio(event.target.value);
+    };
+  
+    const handleDataFimChange = (event) => {
+      setDataFim(event.target.value);
+    };
+  
+    const handleNomeOperadorTransacaoChange = (event) => {
+      setNomeOperadorTransacao(event.target.value);
+    };
+
+    const handleSearch = () => {
+      const formattedDataInicio = dataInicio ? format(new Date(dataInicio), 'dd-MM-yyyy') + ' 00:00:00' : '';
+      const formattedDataFim = dataFim ? format(new Date(dataFim), 'dd-MM-yyyy') + ' 00:00:00' : '';
+      if (isAccountIdValid) {
+        onSearch(accountId, formattedDataInicio, formattedDataFim, nomeOperadorTransacao);
+      };
+  };
+
   return (
     <div className="search-component mt-4 mx-4">
-      <div className="row align-items-center">
+      <h2>Desafio Supera</h2>
+      <div className='row align-items-center'>
+        <div className='conta-id'>
+          <label htmlFor="accountId">ID da Conta:</label>
+          <input
+            type="text"
+            id="accountId"
+            value={accountId}
+            onChange={handleInputChange}
+            style={{ width: '200px' }}
+        />
+        </div>
         <div className="col">
           <label htmlFor="data-inicio">Data de Início</label>
-          <input type="date" id="data-inicio" className="form-control form-control-sm" placeholder="Data inicial" />
+          <input
+            type="date"
+            id="data-inicio"
+            className="form-control form-control-sm"
+            placeholder="Data inicial"
+            value={dataInicio}
+            onChange={handleDataInicioChange}
+          />
         </div>
         <div className="col">
           <label htmlFor="data-fim">Data de Fim</label>
-          <input type="date" id="data-fim" className="form-control form-control-sm" placeholder="Data final" />
+          <input
+            type="date"
+            id="data-fim"
+            className="form-control form-control-sm"
+            placeholder="Data final"
+            value={dataFim}
+            onChange={handleDataFimChange}
+          />
         </div>
         <div className="col">
           <label htmlFor="nome-operador">Nome do Operador Transacionado</label>
-          <input type="text" id="nome-operador" className="form-control form-control-sm" placeholder="Nome do Operador" />
+          <input
+            type="text"
+            id="nome-operador"
+            className="form-control form-control-sm"
+            placeholder="Nome do Operador"
+            value={nomeOperadorTransacao}
+            onChange={handleNomeOperadorTransacaoChange}
+          />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Pesquisar</button>
+          <button className="btn btn-primary" onClick={handleSearch} disabled={!accountId}>
+            Pesquisar
+          </button>
         </div>
       </div>
     </div>
